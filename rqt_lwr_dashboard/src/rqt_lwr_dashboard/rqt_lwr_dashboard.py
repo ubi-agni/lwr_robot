aa4f04efa93794b9e22df711a0d50988a295fc14
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2016-03-23 23:49:45 +0100

    Added a rqt dashboard

---
+++ rqt_lwr_dashboard/src/rqt_lwr_dashboard/rqt_lwr_dashboard.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# derived from rqt_emergency_buttons: emergency_buttons_dashboard.py
+#   original Authors Sammy Pfeiffer
+#
+# Copyright (c) 2015 PAL Robotics SL. All Rights Reserved
+#
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that the
+# above copyright notice and this permission notice appear in all
+# copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+#
+
+# author Guillaume WALCK (2016)
+
+import rospy
+import functools
+from rqt_robot_dashboard.dashboard import Dashboard
+
+from python_qt_binding.QtCore import QSize
+from QtGui import QPushButton, QVBoxLayout, QHBoxLayout, QWidget, \
+    QCheckBox
+
+from lwr_dashboard.lwr_dashboard import LwrDashboard
+
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
+
+from .state_button import ControlStateButton
+
+
+class RqtLwrDashboard(Dashboard):
+    """
+    Dashboard for Mekabot
+
+    :param context: the plugin context
+    :type context: qt_gui.plugin.Plugin
+    """
+    def setup(self, context):
+        self.name = 'Emergency Buttons Dashboard'
+        self.max_icon_size = QSize(50, 30)
+
+        self._last_dashboard_message_time = rospy.Time.now()
+        self._state_buttons = {}
+        self._widget_initialized = False
+        self._last_error = None
+        self._last_power_state = None
+        self._last_control_state = None
+
+        # TODO read this list on the parameters
+        group_names = ["left_arm", "right_arm"]
+
+        self._lwrdb = {}
+        self._lwrdb["left_arm"] = LwrDashboard(namespace="la")
+        self._lwrdb["right_arm"] = LwrDashboard(namespace="ra")
+
+        # create as many buttons as groups received
+        for group_name in group_names:
+            self._state_buttons[group_name] = ControlStateButton(group_name, self)
+
+        self._main_widget = QWidget()
+        vlayout = QVBoxLayout()
+        hlayout = QHBoxLayout()
+
+        self.chk_all = QCheckBox("enable_all")
+        self.chk_all.setChecked(True)
+
+        self.btn_command_mode = QPushButton("command_mode")
+        self.btn_monitor_mode = QPushButton("monitor_mode")
+
+        self.btn_command_mode.setEnabled(False)
+        self.btn_monitor_mode.setEnabled(False)
+
+        hlayout.addWidget(self.chk_all)
+
+        vlayout.addLayout(hlayout)
+        vlayout.addWidget(self.btn_command_mode)
+        vlayout.addWidget(self.btn_monitor_mode)
+
+        self.btn_command_mode.clicked.connect(self.on_btn_command_mode_clicked)
+        self.btn_monitor_mode.clicked.connect(self.on_btn_monitor_mode_clicked)
+        self.chk_all.stateChanged.connect(self.on_enable_all_clicked)
+
+        self._main_widget.setLayout(vlayout)
+        self.context.add_widget(self._main_widget)
+        # self._main_widget.addLayout(hlayout)
+        self._widget_initialized = True
+
+        self._diag_subs = {}
+        self._diag_subs["left_arm"] = rospy.Subscriber("/la/diagnostics/", DiagnosticArray,
+                                                       functools.partial(self.diag_callback, group_name="left_arm"))
+        self._diag_subs["right_arm"] = rospy.Subscriber("/ra/diagnostics/", DiagnosticArray,
+                                                        functools.partial(self.diag_callback, group_name="right_arm"))
+
+    def on_enable_all_clicked(self):
+        """
+        start
+        """
+        for group_name in self._state_buttons:
+            self._state_buttons[group_name].enable_menu.setChecked(self.chk_all.isChecked())
+            self._state_buttons[group_name].enable_menu.triggered.emit(self.chk_all.isChecked())
+            # stateChanged.emit( _set_enabled_signal.emit(self.chk_all.isChecked())
+
+    def on_btn_command_mode_clicked(self, group_name=None):
+        """
+        switch to command mode
+        :param group_name: group concerned, default is None meaning act on all enabled groups
+
+        """
+        if group_name is not None:
+            if self._state_buttons[group_name].enable_menu.isChecked():
+                self._lwrdb[group_name].command_mode_request()
+        else:
+            for group_name in self._state_buttons:
+                if self._state_buttons[group_name].enable_menu.isChecked():
+                    self._lwrdb[group_name].command_mode_request()
+
+    def on_btn_monitor_mode_clicked(self, group_name=None):
+        """
+        switch to monitor mode
+        :param group_name: group concerned, default is None meaning act on all enabled groups
+        """
+        if group_name is not None:
+            if self._state_buttons[group_name].enable_menu.isChecked():
+                self._lwrdb[group_name].monitor_mode_request()
+        else:
+            for group_name in self._state_buttons:
+                if self._state_buttons[group_name].enable_menu.isChecked():
+                    self._lwrdb[group_name].monitor_mode_request()
+
+    def get_widgets(self):
+        widgets_list = []
+        for group_name in self._state_buttons:
+            widgets_list.append([self._state_buttons[group_name]])
+
+        return widgets_list
+
+    def diag_callback(self, msg, group_name):
+        """
+        callback to process lwr diagnostic messages
+        :param msg:
+        :type msg: Diagnostic
+        """
+        
+        power_state = None
+        control_strategy = None
+        control_state = None
+        error = False
+        if group_name in self._state_buttons:
+            if len(msg.status) > 1:
+                if msg.status[1].name == "lwr robot state":
+                    for prop in msg.status[1].values:
+                        if "Power" in prop.key:
+                            power_state = prop.value
+                        if "Control Strategy" in prop.key:
+                            control_strategy = prop.value
+                    if msg.status[1].level == DiagnosticStatus.ERROR:
+                        error = True
+                if msg.status[0].name == "lwr FRI state":
+                    for prop in msg.status[0].values:
+                        if "State" in prop.key:
+                            control_state = prop.value
+
+            if error:
+                self._state_buttons[group_name].set_state("error")
+            else:
+                if power_state is not None and control_state is not None:
+                    if power_state == "0000000":
+                        self._state_buttons[group_name].set_state("monitor_off")
+                    elif power_state == "1111111" and control_state == "monitor":
+                        self._state_buttons[group_name].set_state("monitor_on")
+                    elif power_state == "1111111" and control_state == "command":
+                        self._state_buttons[group_name].set_state("command")
+                    else:
+                        print "Invalid states power:", power_state, ", control_state:", control_state
+            self._last_error = error
+            self._last_power_state = power_state
+            self._last_control_state = control_state
+
+    #def shutdown_dashboard(self):
---
+++ rqt_lwr_dashboard/src/rqt_lwr_dashboard/state_button.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+#
+# author Guillaume Walck (2015)
+#
+# derived from pr2_breaker.py
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import rospy
+import functools
+from python_qt_binding.QtCore import QSize, pyqtSignal
+
+from rqt_robot_dashboard.widgets import MenuDashWidget
+
+from python_qt_binding.QtGui import QMessageBox
+
+
+class ControlStateButton(MenuDashWidget):
+    """
+    Dashboard widget to display and interact with the lwr state.
+    """
+    # create a signal for external triggering
+    _set_enabled_signal = pyqtSignal(int)
+
+    def __init__(self, group_name, parent):
+        """
+        :param group_name: Name of the group
+        :type group_name: str
+        :param group_index: Index of the group
+        :type group_index: int
+        """
+        
+        self._serial = 0
+        self._parent = parent
+        self._name = group_name
+
+        if group_name == 'left_arm':
+            state_icon = 'ic-larm.svg'
+        elif group_name == 'right_arm':
+            state_icon = 'ic-rarm.svg'
+        else:
+            state_icon = 'ic-breaker.svg'
+
+        command_icon = ['bg-green.svg', state_icon]
+        monitor_on_icon = ['bg-yellow.svg', state_icon]
+        monitor_off_icon = ['bg-grey.svg', state_icon]
+        error_icon = ['bg-red.svg', state_icon, 'ol-err-badge.svg']
+        disabled_icon = ['bg-light_grey.svg', state_icon]
+
+        icons = [disabled_icon, error_icon, monitor_off_icon, monitor_on_icon, command_icon]
+        self._state_dict= {"disabled": 0, "error": 1, "monitor_off": 2, "monitor_on": 3, "command": 4}
+
+        super(ControlStateButton, self).__init__('State:' + group_name, icons=icons, icon_paths=[['rqt_lwr_dashboard', 'images']])
+
+        # init the button in disabled
+        self.update_state(0)
+
+        self.setFixedSize(self._icons[0].actualSize(QSize(50, 30)))
+
+        self.add_action('Command', functools.partial(self._parent.on_btn_command_mode_clicked, group_name=self._name))
+        self.add_action('Monitor', functools.partial(self._parent.on_btn_monitor_mode_clicked, group_name=self._name))
+        self.enable_menu = self.add_action('Enable/Disable', self.on_enable_disable)
+        self.enable_menu.setCheckable(True)
+        self.enable_menu.setChecked(False)
+        self.set_group_enabled(False)
+
+        self._set_enabled_signal.connect(self.on_enable_disable)
+
+        
+        self._pending_msg =  None
+        self._state = None
+        self._last_status_msg = None
+        self.setToolTip(group_name)
+
+    def on_enable_disable(self):
+        if self.enable_menu.isChecked():
+            if self._state is not None:
+                self._state = None
+                self.set_group_enabled(True)
+                self.set_state(self._pending_msg)
+            else:
+                self.enable_menu.setChecked(False)
+        else:
+            self.set_group_enabled(False)
+
+    def set_run(self):
+        if (not self.control(self._name, 2)):
+            return
+
+        self.control(self._name, 3)
+
+    def set_freeze(self):
+        self.control(self._name, 2)
+
+    def set_instandby(self):
+        self.control(self._name, 1)
+
+    def set_run_all(self):
+        if (not self.control3(2)):
+            return
+        self.control3(3)
+
+    def set_freeze_all(self):
+        self.control3(2)
+
+    def set_standby_all(self):
+        self.control3(1)
+
+    def set_state(self, state):
+        """
+        Sets state of button based on msg
+
+        :param msg: message containing the M3 control state
+        :type msg: m3meka_msgs.msg.M3ControlStates
+        """
+
+        if (self.enable_menu.isChecked() or self._state is None):
+            status_msg = "Running"
+            # if first message received, enable the group
+            if self._state is None:
+                self.set_group_enabled(True)
+                self.enable_menu.setChecked(True)
+            if self._state != state:
+                self._state = state
+                if state in self._state_dict:
+                    self.update_state(self._state_dict[state])
+
+                if (status_msg != self._last_status_msg):
+                    self.setToolTip("Group: %s \nState: %s" % (self._name, status_msg))
+                    self._last_status_msg = status_msg
+        else:
+            self._pending_msg = state
+
+    def set_group_enabled(self, val):
+        if not val:
+            self.update_state(0)
+        for action in self._menu.actions():
+            if not action.isCheckable():
+                action.setEnabled(val)
+
